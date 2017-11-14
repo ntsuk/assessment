38e96b6622379388a4993bfb039501dd594eda3e
ntsuk <33646139+ntsuk@users.noreply.github.com>
2017-11-14 12:24:09 +0900

    Create assessment.js

---
+++ assessment.js
@@ -0,0 +1,92 @@
+(function(){
+  'use strict';
+  const userNameInput = document.getElementById('user-name');
+  const assessmentButton = document.getElementById('assessment');
+  const resultDivided = document.getElementById('result-area');
+  const tweetDivided = document.getElementById('tweet-area');
+  userNameInput.onkeydown = (event) =>{
+    if(event.keyCode === 13){
+      makeAssessment();
+    }
+  }
+  assessmentButton.onclick = () => {
+    makeAssessment();
+  }
+  //診断結果を作成する
+  function makeAssessment(){
+      const userName = userNameInput.value;
+       /**
+     *  指定した要素の子どもをすべて削除する
+     *  @param{HTMLElement} element HTMLの要素
+      */
+      function removeAllChildren(element){
+        while(element.firstChild){
+          element.removeChild(element.firstChild);
+        }
+      }
+      if(userName.length === 0){ //名前が空の場合は処理は終了
+        return;
+      }
+      //診断結果エリアの作成
+     removeAllChildren(resultDivided); //結果エリアに子要素が存在した場合削除
+      const header = document.createElement('h3');
+      header.innerText = '診断結果';
+      resultDivided.appendChild(header);
+
+      const paragraph = document.createElement('p');
+      const result = assessment(userName);
+      paragraph.innerText = result;
+      resultDivided.appendChild(paragraph);
+
+      // ツイートエリアの作成
+      removeAllChildren(tweetDivided);
+      const anchor = document.createElement('a');
+      const hrefValue = 'https://twitter.com/share?ref_src=twsrc%5Etfw';
+      anchor.setAttribute('href', hrefValue);
+      anchor.className = 'twitter-share-button';
+      anchor.dataset.text = result;
+      anchor.dataset.hashtags = 'あなたのいいところ診断';
+      anchor.dataset.lang = 'ja';
+      anchor.dataset.showCount = false;
+      anchor.innerText = '診断結果をツイートする';
+      tweetDivided.appendChild(anchor);
+      twttr.widgets.load();
+  }
+  const answers = [
+    '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
+    '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
+    '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
+    '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
+    '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
+    '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
+    '{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
+    '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
+    '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
+    '{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
+    '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
+    '{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
+    '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
+    '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
+    '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
+    '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。'
+  ];
+  /**
+    * 名前の文字列を渡すと診断結果を返す関数
+    *  @param{string} userName
+    *  @return{string} 診断結果
+  */
+  function assessment(userName){
+    // 全文字のコード番号を取得してそれを足し合わせる
+    let sumOfcharCode = 0;
+    for(let i = 0; i < userName.length; i++){
+      sumOfcharCode = sumOfcharCode + userName.charCodeAt(i);
+    }
+    // 文字のコード番号の合計を回数数で割って添え字の数値を求める
+    const answersNum = answers.length;
+    const index = sumOfcharCode % answersNum;
+    let result = answers[index];
+    // {userName}をユーザーの名前に置き換える
+    result = result.replace(/\{userName\}/g, userName);
+    return result;
+  }
+})();
